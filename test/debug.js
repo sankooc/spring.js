346b27faa0419d051596a2a61056935229461f54
sankooc park <piaochangguo@yunji.ai>
2019-08-21 16:43:19 +0800

    fix: 直接添加配置文件

--- test/debug.js
+++ test/debug.js
@@ -16,39 +16,3 @@ const start = async () => {
 
 
 (start)()
-
-// const smp = springJS.get()
-// const resolve
-// const route = () => { }
-
-// const load = (cfg) => {
-//   const arrs = Object.keys(cfg).map(c => service(cfg[c], c));
-//   LOG(arrs)
-// };
-
-
-// const cfg = {
-//   s1: __dirname + '/demo.js',
-//   s2: __dirname + '/demo2.js',
-// };
-
-// load(cfg)
-
-// const parsed = [{
-//     type: 'ecreate',
-//     name: 's1',
-//     filename: '/Users/yj431/work/spring.js/test/demo.js',
-//     isAsync: true,
-//     params: []
-// },
-// {
-//     type: 'exports',
-//     name: 's2',
-//     filename: '/Users/yj431/work/spring.js/test/demo2.js',
-//     params: ['s1']
-// }];
-
-
-// (container.build.bind(container, parsed))();
-// const pm = parse1(__dirname + '/router.js', 'demo', { router: 1 });
-// console.log(pm);
